fbbe6a1e419e601617746e65d2824ebdb67b732e
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-03-14 18:07:36 +0300

    Add the MainHeader & the Navigation components

---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,25 @@
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => (
+  <nav className={classes.nav}>
+    <ul>
+      {props.isLoggedIn && (
+        <li>
+          <a href="/">Users</a>
+        </li>
+      )}
+      {props.isLoggedIn && (
+        <li>
+          <a href="/">Admin</a>
+        </li>
+      )}
+      {props.isLoggedIn && (
+        <li>
+          <button type='button' onClick={props.onLogout}>Logout</button>
+        </li>
+      )}
+    </ul>
+  </nav>
+);
+
+export default Navigation;
